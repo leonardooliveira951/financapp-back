1eb9300e86405d7456188a7d34151fd4eadbeafa
Leonardo Oliveira <leonardo.oliveira951@gmail.com>
2021-12-12 22:12:15 -0300

    [feat] Criado PaymentController com método getSummaryByDate

---
+++ lumen/app/Http/Controllers/PaymentController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\PaymentService;
+use Exception;
+use Illuminate\Http\Request;
+
+
+class PaymentController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getSummaryByDate(Request $request)
+    {
+        try {
+            $response = PaymentService::getSummaryByDate($request);
+            if ($response == null){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Nenhum pagamento localizado.'
+                ], 404
+                );
+            }
+            return response()->json([
+                'status' => true,
+                'message' => 'Resumo de pagamentos listado com sucesso',
+                'payment_summary' => $response,
+            ], 200
+            );
+        } catch (Exception $e) {
+            $message = "Erro ao listar resumo de pagamentos: ". $e->getMessage();
+            return response()->json([
+                'status' => false,
+                'message' => $message
+            ],500
+            );
+        }
+    }
+
+}
