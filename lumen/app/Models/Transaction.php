5f672c78e6e7d403080bedbc274ba0886b614ee8
Leonardo Oliveira <leonardo.oliveira951@gmail.com>
2021-12-15 01:21:01 -0300

    [feat] Adicionados campos editáveis ao atributo $fillable do Transaction Model

--- lumen/app/Models/Transaction.php
+++ lumen/app/Models/Transaction.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = ['description', 'amount', 'installments', 'category_id', 'invoice_first_charge'];
 }
